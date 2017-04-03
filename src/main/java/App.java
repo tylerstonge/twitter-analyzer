708e40ac49a1a6a388ce8652a986ec8e54e6174b
Tyler St. Onge <tylertstonge@gmail.com>
2017-04-03 18:58:16 -0400

    added sentiment analysis

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         TwitterReader reader = new TwitterReader();
-        List<Tweet> tweets = reader.getTweetsFromUser("potus", 1);
+        List<Tweet> tweets = reader.getTweetsFromUser("potus", 20);
         SentimentAnalyzer sa = SentimentAnalyzer.getInstance();
         for (Tweet t : tweets) {
             System.out.println(t.getText() + " - " + sa.classify(t.getText()));
--- src/main/java/SentimentAnalyzer.java
+++ src/main/java/SentimentAnalyzer.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import com.aliasi.classify.LMClassifier;
 import com.aliasi.classify.DynamicLMClassifier;
@@ -8,34 +10,35 @@ import com.aliasi.classify.Classified;
 import com.aliasi.classify.Classification;
 import com.aliasi.util.AbstractExternalizable;
 import com.aliasi.classify.ConditionalClassification;
+import com.aliasi.corpus.ObjectHandler;
 
 public class SentimentAnalyzer {
-    
+
     private static SentimentAnalyzer instance;
-    
-    LMClassifier class;
+
+    LMClassifier classifier;
     String[] categories;
-    
+
     public SentimentAnalyzer() {
         // Train the classifier
         try {
             train();
-            this.class = (LMClassifier) AbstractExternalizable.readObject(getClass().getClassLoader().getResource("sentiment_data").getFile());
-            this.categories = class.categories();
+            this.classifier = (LMClassifier) AbstractExternalizable.readObject(new File("classifier.txt"));
+            this.categories = this.classifier.categories();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-    
+
     public String classify(String text) {
-        ConditionalClassification classification = class.classify(text);
+        ConditionalClassification classification = this.classifier.classify(text);
         return classification.bestCategory();
     }
-    
+
     public void train() throws IOException, ClassNotFoundException {
-        File folder = getClass().getClassLoader().getResource("sentiment_data").getFile();
+        File folder = new File(getClass().getClassLoader().getResource("sentiment_data").getFile());
         String[] cats = folder.list();
         int nGram = 7;
         LMClassifier classifier = DynamicLMClassifier.createNGramProcess(cats, nGram);
@@ -43,21 +46,23 @@ public class SentimentAnalyzer {
             String cat = cats[i];
             Classification classification = new Classification(cat);
             File file = new File(folder, cats[i]);
-            File trainFiles = file.listFiles();
+            File[] trainFiles = file.listFiles();
             for (int j = 0; j < trainFiles.length; j++) {
                 File trainFile = trainFiles[i];
-                String review = Files.readFromFile(trainFile, "ISO-8859-1");
+                //String review = Files.readAllLines((Path) trainFile.getPath(), "ISO-8859-1");
+                byte[] r = Files.readAllBytes(Paths.get(trainFile.getPath()));
+                String review = new String(r, "ISO-8859-1");
                 Classified classified = new Classified(review, classification);
-                ((ObjectHandler>) classifier).handle(classified);
+                ((ObjectHandler) classifier).handle(classified);
             }
         }
-        AbstractExternalizable.compileTo((Compilable) classifier, new File("classifier.txt")); 
+        AbstractExternalizable.compileTo((Compilable) classifier, new File("classifier.txt"));
     }
-    
+
     public static SentimentAnalyzer getInstance() {
         if (instance == null)
             instance = new SentimentAnalyzer();
         return instance;
     }
-    
+
 }
